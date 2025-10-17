34e21f5eba4859db3d3b8cea07a40311a70b850b
Giddy-K <gideonkipamet@gmail.com>
2025-10-17 03:48:01 +0300

    More modification to the base header

--- DirectXGame/DX3D/Include/DX3D/Core/Base.h
+++ DirectXGame/DX3D/Include/DX3D/Core/Base.h
@@ -8,5 +8,11 @@ namespace dx3d
 		public:
 			Base();
 			virtual ~Base();
+
+	protected:
+		Base(const Base&) = delete;
+		Base& operator=(const Base&) = delete;
+		Base(Base&&) = delete;
+		Base& operator=(Base&&) = delete;
 	};
 }

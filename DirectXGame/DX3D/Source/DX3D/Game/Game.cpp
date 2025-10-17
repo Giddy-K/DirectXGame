805ad14e7c775499a4e11bf68b516a360a82f564
Giddy-K <gideonkipamet@gmail.com>
2025-10-17 03:46:55 +0300

    Modification to the game class to include graphics engine

--- DirectXGame/DX3D/Source/DX3D/Game/Game.cpp
+++ DirectXGame/DX3D/Source/DX3D/Game/Game.cpp
@@ -1,8 +1,11 @@
 #include <DX3D/Game/Game.h>
 #include <DX3D/Window/Window.h>
+#include <DX3D/Graphics/GraphicsEngine.h>
 
 dx3d::Game::Game()
 {
+	//Last to be created, first to be destroyed
+	m_graphicsEngine = std::make_unique<GraphicsEngine>();
 	m_display = std::make_unique<Window>();
 }
 

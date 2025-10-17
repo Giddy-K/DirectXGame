315643839c113a2f6e434c3f008d20e4219b561a
Giddy-K <gideonkipamet@gmail.com>
2025-10-17 06:55:03 +0300

    Update Base.h

--- DirectXGame/DX3D/Include/DX3D/Core/Base.h
+++ DirectXGame/DX3D/Include/DX3D/Core/Base.h
@@ -1,18 +1,27 @@
 #pragma once
-
+#include <DX3D/Core/Common.h>
 
 namespace dx3d
 {
 	class Base
 	{
 		public:
-			Base();
+			explicit Base(const BaseDesc& desc); //Only takes one parameter
 			virtual ~Base();
+			virtual Logger& getLogger() const noexcept final; //does not modify the internal state of the class, noexcept guarantees it won't throw exceptions, final prevents further overrides in derived classes
+
 
 	protected:
 		Base(const Base&) = delete;
 		Base& operator=(const Base&) = delete;
 		Base(Base&&) = delete;
 		Base& operator=(Base&&) = delete;
+
+	protected:
+		// Reference to the global logger instance depenency injection pattern
+		// This assumes that a global logger instance exists and is accessible
+		// throughout the application lifecycle.
+		Logger& m_logger;
+
 	};
 }
